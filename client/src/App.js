12b586e15e53c834b57d4e667a90bc3606277385
Amardeep T <amardeept@annait.net>
2020-07-25 19:57:25 +0530

    cleaned up react project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is demo react project
+    </div>
+  );
+}
+
+export default App;
