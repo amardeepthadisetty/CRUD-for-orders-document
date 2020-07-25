5871860fd88bcd01b5b249cf558b1301cce9233a
Amardeep T <amardeept@annait.net>
2020-07-25 20:22:36 +0530

    basic dev extreme is working

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,41 @@ import 'devextreme/dist/css/dx.light.css';
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      This is demo react project
-    </div>
-  );
+import Chart, {
+  ArgumentAxis,
+  Series,
+  Legend
+} from 'devextreme-react/chart';
+
+
+import Button from 'devextreme-react/button';
+
+const data = [{
+  arg: 1990,
+  val: 5320816667
+}, {
+  arg: 2000,
+  val: 6127700428
+}, {
+  arg: 2010,
+  val: 6916183482
+}];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+
+        <Chart dataSource={data}>
+          <ArgumentAxis tickInterval={10} />
+          <Series type="bar" />
+          <Legend visible={false} />
+        </Chart>
+
+      </div>
+      
+    );
+  }
 }
 
 export default App;
